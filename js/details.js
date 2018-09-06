07468fdddd76e3bde3862c7b0d1eb2ac79f3ed74
gaobaiqiqiu <294002670@qq.com>
2018-09-06 16:53:21 +0800

    18.9.6更新内容滑动

--- js/details.js
+++ js/details.js
@@ -20,6 +20,7 @@ var dynastyId = GetQueryString("dynastyId");
 
 var dynastyList;  //朝代列表
 var mySwiper;  //左右滑动
+var swiperP; //P标签滑动
 var shouSwiperIndex = 0;  //朝代的索引
 var SwiperList = {};  //将所有的朝代按朝代ID值存储到一个对象中
 var sonIndex = 0;  //左右滑动时准确的拿到所滑到的朝代当先显示的内容的索引
@@ -66,7 +67,6 @@ $(function(){
                 //获取session中的值
                 // var initIndex = sessionStorage.getItem(dynastyList[i]["dynastyId"] );
                 
-
                 $.ajax({
                     type: "post",
                     url: "http://www.dadpat.com/dynastyInfo/getAllList.do",
@@ -89,22 +89,29 @@ $(function(){
                         if( chaoDaiIndex < 0 ){
                             return;
                         }
-                        swiperAllIn.push(chaodaiData[0]["dynastyDescp"].split(";")[0]);
-                        swiperAll.push(chaodaiData[0]["dynastyDescp"].split(";")[1]);
-                        chaodaiList[dynastyList[chaoDaiIndex]["dynastyId"]].find("#swiper1" + dynastyList[chaoDaiIndex]["dynastyId"] + ">.swiper-wrapper").append($("#template1").html().replace("$img$","http://www.dadpat.com/"+chaodaiData[0]["image"][0]["imageUrl"]).replace("$content$",chaodaiData[0]["dynastyDescp"].split(";")[0]).replace("$audio$","http://www.dadpat.com/"+chaodaiData[0]['audio'][0]['attUrl']));
-                        for( var j = 1; j < chaodaiData.length; ++j ){
-                            chaodaiList[dynastyList[chaoDaiIndex]["dynastyId"]].find("#swiper1" + dynastyList[chaoDaiIndex]["dynastyId"] + ">.swiper-wrapper").append($("#template1").html().replace("$img$","http://www.dadpat.com/"+chaodaiData[j]["image"][0]["imageUrl"]).replace("$content$",chaodaiData[j]["dynastyDescp"].split(";")[0]).replace("$audio$","http://www.dadpat.com/"+chaodaiData[j]['audio'][0]['attUrl']));
+                        // swiperAllIn.push(chaodaiData[0]["dynastyDescp"].split(";")[0]);
+                        // swiperAll.push(chaodaiData[0]["dynastyDescp"].split(";")[1]);
+                        // chaodaiList[dynastyList[chaoDaiIndex]["dynastyId"]].find("#swiper1" + dynastyList[chaoDaiIndex]["dynastyId"] + ">.swiper-wrapper").append($("#template1").html().replace("$img$","http://www.dadpat.com/"+chaodaiData[0]["image"][0]["imageUrl"]).replace("$content$", chaodaiData[0]["dynastyDescp"].split(";")[0]).replace("$audio$","http://www.dadpat.com/"+chaodaiData[0]['audio'][0]['attUrl']));
+                        for( var j = 0; j < chaodaiData.length; ++j ){
+                            chaodaiList[dynastyList[chaoDaiIndex]["dynastyId"]].find("#swiper1" + dynastyList[chaoDaiIndex]["dynastyId"] + ">.swiper-wrapper").append($("#template1").html().replace("$img$","http://www.dadpat.com/"+chaodaiData[j]["image"][0]["imageUrl"]).replace("$content$", chaodaiData[j]["dynastyDescp"].split(";")[0]).replace("$audio$","http://www.dadpat.com/"+chaodaiData[j]['audio'][0]['attUrl']));
                             swiperAllIn.push(chaodaiData[j]["dynastyDescp"].split(";")[0]);
                             swiperAll.push(chaodaiData[j]["dynastyDescp"].split(";")[1]);
-                            console.log(chaodaiData[j].audio.length)
                         }
-                        $('.chaoDaiBox .swiper-wrapper').css({height:docuHeight+"px"});
-                        $('.chaoDaiBox .swiper-slide').css({height:docuHeight+"px"});
+                        $('.chaoDaiBox #swiper1' + dynastyList[chaoDaiIndex]["dynastyId"]).css({height:'100%'});
+                        // $('#swiper-container2 .swiper-wrapper').css({height:'100%'});
                         //点击朝代进入时，点击音频按钮，播放当前显示的语音
                         if( shouSwiperIndex == chaoDaiIndex ) {        
                             loadPage( initIndex != null && shouSwiperIndex == chaoDaiIndex ? initIndex : 0 );                            
                         }
 
+                        //p标签的滑动
+                        swiperP = new Swiper('#swiper1'+ dynastyId +' .swiper-container', {
+                            direction: 'vertical',
+                            slidesPerView: 'auto',
+                            touchMoveStopPropagation : false,
+                            freeMode: true,
+                            mousewheel: true,
+                        });
                     }
                 });
                 //上下滑动
@@ -119,8 +126,7 @@ $(function(){
                     },
                     on:{
                         slideChangeTransitionStart: function () {
-
-                            //只能存储当前朝代，左右滑动到其它朝代时不会往session里存
+                           //只能存储当前朝代，左右滑动到其它朝代时不会往session里存
                             /*if( dynastyList[shouSwiperIndex]["dynastyId"] == dynastyId ){
                                 //往session中存储
                                 sessionStorage.setItem( dynastyList[shouSwiperIndex]["dynastyId"], this.activeIndex );
@@ -139,14 +145,16 @@ $(function(){
                                 }
                             }
                             samllIndex = this.activeIndex;
+                            console.log(this.activeIndex)
                             loadPage(samllIndex);
+
 
                         },
                         slideChangeTransitionEnd: function () {
 
                         }
                     }
-                });
+                });                
             }
             /*左右滑动*/
             mySwiper = new Swiper('#swiper-container', {
@@ -180,10 +188,15 @@ $(function(){
                 }
             });
             mySwiper.slideTo( shouSwiperIndex, 200, false );
+           
         }
     });
     
 });
+
+
+
+
 /*点击返回时空隧道页*/
 $('.back2').click(function(){
     window.location.href="tunnel.html";
@@ -194,7 +207,13 @@ $('.audio').click(function(){
     var playEtextText = playEtext[0].innerText;
     if( playAudio[0].paused ){
         playAudio[0].play();
-        _processK();
+        console.log(playAudio[0].paused )
+        if(playAudio[0].paused){
+            console.log('不触发歌词')
+        }else{
+            _processK();
+        }
+        
     }else{
         playAudio[0].pause();
         playAudio[0].load();
@@ -202,24 +221,28 @@ $('.audio').click(function(){
     }
 });
 
-$(".chaoDaiFu").scroll(function() {
-    return false;
-  });
-
+//scroll() 方法触发 scroll 事件，或规定当发生 scroll 事件时运行的函数。
+// $("#swiper-container2").scroll(function() {
+//     return false;
+//   });
 function loadPage( index ) {
     if( !mySwiper ) {
         return;
     }
     sonIndex = index;
-    // console.log("大swiper索引值："+shouSwiperIndex+",小swiper索引值:"+sonIndex);
-    var _etext = $("#swiper-container .swiper-container:eq(" + shouSwiperIndex +  ") .swiper-wrapper>div:eq(" + sonIndex +  ") .chaoDaiFu");
-    var _audio = $("#swiper-container .swiper-container:eq(" + shouSwiperIndex +  ") .swiper-wrapper>div:eq(" + sonIndex +  ") audio");
+    console.log('上下滑动索引:'+sonIndex+',左右滑动索引:'+shouSwiperIndex);
+    // var _etext = $("#swiper-container .swiper-container:eq(" + shouSwiperIndex +  ") .swiper-wrapper>div:eq(" + sonIndex +  ") #swiper-container2");
+    // var _audio = $("#swiper-container .swiper-container:eq(" + shouSwiperIndex +  ") .swiper-wrapper>div:eq(" + sonIndex +  ") audio");
+    var _etext = $("#swiper-container .swiperIndex:eq(" + shouSwiperIndex +  ") .swiper-wrapper>div:eq(" + sonIndex +  ") #swiper-container2");
+    var _audio = $("#swiper-container .swiperIndex:eq(" + shouSwiperIndex +  ") .swiper-wrapper>div:eq(" + sonIndex +  ") audio");
+    console.log(_etext)
     playEtext = _etext;
     playAudio = _audio;
     if( _etext.length != 1 || _audio.length != 1 ) {
         return;
     }
     var innText = _etext[0]['innerText'];
+    console.log(innText)
     for(var i=0;i< swiperAll.length;i++){
         if(innText == swiperAllIn[i]){
             /*================================================制作歌词 - 开始======================================================================================*/
@@ -243,11 +266,12 @@ function loadPage( index ) {
                 item["d"] = temp[2];
                 lines[0]["ws"].push(item);
             }
-            var pObj = lines[0]['ws'];
             var pOb = '';
+            var pObj = lines[0]['ws'];
             for(var k=0;k<pObj.length;k++){
                 pOb += "<p>" + pObj[k]['w'] + "</p>";
             }
+            // console.log(pOb)
             var wstoes = function(words) {
                 var ps = "";
                 for (var i = 0; i < words.length; i++) {
